8741968a10bdce73df90d789b970b0aceeaa6350
William Phipps <willcphipps@gmail.com>
2020-04-14 20:01:09 -0500

    adding form validations

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,27 +3,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using FormSubmission.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace FormSubmission.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace FormSubmission.Controllers {
+    public class HomeController : Controller {
+        [HttpGet ("")]
+        public IActionResult Index () {
+            return View ("Index");
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        [HttpPost ("Create")]
+        public IActionResult Create (User user) {
+            if (ModelState.IsValid) {
+                // do somethng!  maybe insert into db?  then we will redirect
+                return RedirectToAction ("Success");
+            } else {
+                // Oh no!  We need to return a ViewResponse to preserve the ModelState, and the errors it now contains!
+                return View ("Index");
+            }
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpGet ("Success")]
+        public IActionResult Success() {
+            return View ();
         }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+namespace FormSubmission.Models {
+    public class User {
+        [Required (ErrorMessage = "Name Must be more than 4 characters")]
+        [MinLength (4)]
+        public string FirstName { get; set; }
+
+        [Required (ErrorMessage = "Last name must be more then 4 characters")]
+        [MinLength (4)]
+        public string LastName { get; set; }
+
+        
+        [Required(ErrorMessage="Only Humans!")]
+        [Range(0,120)]
+        public int Age { get; set; }
+
+        [Required (ErrorMessage = "You must submit an email!")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required (ErrorMessage = "Password must be 8 characters!")]
+        [MinLength (8)]
+        [DataType (DataType.Password)]
+        public string Password { get; set; }
+    }
+}
